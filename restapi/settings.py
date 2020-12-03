dfb91d44561581358ac849578dafd6a5e601e0ed
Praveen Ukkoji <praveenukkoji@gmail.com>
2020-12-03 22:33:17 +0530

    deploy changes

--- restapi/settings.py
+++ restapi/settings.py
@@ -128,7 +128,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Media
 

b11a313e36951929bd82ddb2db4319ab077d4f34
Praveen Ukkoji <praveenukkoji@gmail.com>
2020-12-04 21:27:09 +0530

    branhc_id attribute removed from faculty

--- user/models.py
+++ user/models.py
@@ -1,40 +1,7 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
-
-# student model
-class Students(Base):
-    __tablename__ = 'student'
-
-    student_id = Column(String, primary_key=True)
-    name = Column(String)
-    usn = Column(String)
-    email = Column(String)
-    password = Column(String)
-    branch_id = Column(String)
-    sem = Column(Integer)
-    year = Column(Integer)
-    created_by = Column(String)
-    created_on = Column(DateTime)
-    modified_by = Column(String)
-    modified_on = Column(DateTime)
-
-    def __init__(self, student_id, name, usn, email, password, branch_id, sem, year, created_by, created_on,
-                 modified_by, modified_on):
-        self.student_id = student_id
-        self.name = name
-        self.usn = usn
-        self.email = email
-        self.password = password
-        self.branch_id = branch_id
-        self.sem = sem
-        self.year = year
-        self.created_by = created_by
-        self.created_on = created_on
-        self.modified_by = modified_by
-        self.modified_on = modified_on
 
 
 # faculty model
@@ -43,7 +10,6 @@ class Faculties(Base):
 
     faculty_id = Column(String, primary_key=True)
     name = Column(String)
-    branch_id = Column(String)
     email = Column(String)
     password = Column(String)
     created_by = Column(String)
@@ -51,11 +17,9 @@ class Faculties(Base):
     modified_by = Column(String)
     modified_on = Column(DateTime)
 
-    def __init__(self, faculty_id, name, email, password, branch_id,
-                 created_by, created_on, modified_by, modified_on):
+    def __init__(self, faculty_id, name, email, password, created_by, created_on, modified_by, modified_on):
         self.faculty_id = faculty_id
         self.name = name
-        self.branch_id = branch_id
         self.email = email
         self.password = password
         self.created_by = created_by
--- user/utils.py
+++ user/utils.py
@@ -1,50 +1,4 @@
-from user.models import Students, Faculties
-from branch.models import Branches
-from restapi.connection import DBConnection
-
-
-# get student payload
-def get_student_payload(data, count):
-    payload = []
-    try:
-        for student in data:
-            with DBConnection() as session:
-                try:
-                    query = session.query(Branches).filter(Branches.branch_id == student.branch_id)
-                    data1 = query.all()
-                    if data1:
-                        for branch in data1:
-                            branch_name = branch.branch_name
-                except Exception as e:
-                    print(e)
-                    raise e
-            new_user = {
-                "student_id": student.student_id,
-                "name": student.name,
-                "usn": student.usn,
-                "email": student.email,
-                "branch_name": branch_name,
-                "sem": student.sem,
-                "year": student.year
-            }
-            payload.append(new_user)
-            count += 1
-    except Exception as e:
-        print(e)
-        raise e
-    return payload, str(count) + " student fetched.", count
-
-
-# columns to update
-student_columns = {
-    "name": Students.name,
-    "usn": Students.usn,
-    "email": Students.email,
-    "password": Students.password,
-    "branch_id": Students.branch_id,
-    "sem": Students.sem,
-    "year": Students.year,
-}
+from user.models import Faculties
 
 
 # get faculty payload
@@ -52,21 +6,10 @@ def get_faculty_payload(data, count):
     payload = []
     try:
         for faculty in data:
-            with DBConnection() as session:
-                try:
-                    query = session.query(Branches).filter(Branches.branch_id == faculty.branch_id)
-                    data1 = query.all()
-                    if data1:
-                        for branch in data1:
-                            branch_name = branch.branch_name
-                except Exception as e:
-                    print(e)
-                    raise e
             new_user = {
                 "faculty_id": faculty.faculty_id,
                 "name": faculty.name,
-                "email": faculty.email,
-                "branch_name": branch_name
+                "email": faculty.email
             }
             payload.append(new_user)
             count += 1
@@ -80,6 +23,5 @@ def get_faculty_payload(data, count):
 faculty_columns = {
     "name": Faculties.name,
     "email": Faculties.email,
-    "password": Faculties.password,
-    "branch_id": Faculties.branch_id
+    "password": Faculties.password
 }
